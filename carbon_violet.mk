19647ad39f6401b2feb1b34b7a0d5421901d28f7
Dyneteve <32036579+Dyneteve@users.noreply.github.com>
2020-01-18 14:19:58 +0200

    violet: Enable TARGET_INCREMENTAL_BLOCK_BASED
    Signed-off-by: ugly-kid-af <dwarmachine24@gmail.com>

--- carbon_violet.mk
+++ carbon_violet.mk
@@ -18,8 +18,9 @@ TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_WIFI_EXT := true
 TARGET_INCLUDE_STOCK_ARCORE := true
-$(call inherit-product, vendor/carbon/config/common.mk)
-$(call inherit-product, vendor/carbon/config/gsm.mk)
+TARGET_INCREMENTAL_BLOCK_BASED := true
+IS_PHONE:= true
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := carbon_violet
